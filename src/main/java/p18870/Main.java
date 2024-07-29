c4448585e59a016fe8222a8a9033f388b505c63d
parkSeryu <pjh971212@gmail.com>
2024-07-30 00:18:51 +0900

    feat: p18870 complete

---
+++ src/main/java/p18870/Main.java
@@ -0,0 +1,42 @@
+package p18870;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+//        System.setIn(new FileInputStream("src/main/java/p18870/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            list.add(sc.nextInt());
+        }
+
+        Set<Integer> set = new TreeSet<>(list);
+
+        Map<Integer, Integer> integerMap = new HashMap<>();
+        int index = 0;
+        for (Integer i : set) {
+            integerMap.put(i, index);
+            index++;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (Integer i : list) {
+            sb.append(integerMap.get(i)).append(" ");
+        }
+        System.out.print(sb.toString());
+
+    }
+}
---
+++ src/test/java/p18870/MainTest.java
@@ -0,0 +1,29 @@
+package p18870;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @DisplayName("예제 입력 1 테스트")
+    @Test
+    void test() {
+        // given
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(4);
+        list.add(-10);
+        list.add(4);
+        list.add(-9);
+
+        // when
+        Main.solution(list);
+
+        // then
+    }
+
+}
