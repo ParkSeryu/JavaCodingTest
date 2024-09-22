026787fb9d2d4caef1e3d8b7629ebd2cfc583736
parkSeryu <pjh971212@gmail.com>
2024-09-22 18:51:03 +0900

    feat: p2805 fix

--- src/main/java/binarysearch/p2805/Main.java
+++ src/main/java/binarysearch/p2805/Main.java
@@ -1,42 +1,46 @@
 package binarysearch.p2805;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            System.setIn(new FileInputStream("src/main/java/binarysearch/p2417/input.txt"));
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long M = sc.nextInt();
+        int[] trees = new int[N];
 
-            long N = Long.parseLong(br.readLine().trim());
-            long result = parametric_search(N);
-            bw.write(result + "\n");
-            bw.flush();
-            bw.close();
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 0; i < trees.length; i++) {
+            trees[i] = sc.nextInt();
         }
+
+        System.out.println(parametric_search(M, trees));
     }
 
-    private static long parametric_search(long su) {
+    private static long parametric_search(long height, int[] trees) {
         long start = 0;
         long end = Long.MAX_VALUE;
         long result = 0;
 
         while (start <= end) {
             long middle = (start + end) / 2;
-            if (Math.pow(middle, 2) >= su) {
+            if (getHeight(middle, trees) < height) {
                 end = middle - 1;
-                result = middle;
             } else {
                 start = middle + 1;
+                result = middle;
+            }
+        }
+        return result;
+    }
+
+    private static long getHeight(long height, int[] trees) {
+        long result = 0;
+        for (int tree : trees) {
+            long h = tree - height;
+            if (h > 0) {
+                result += h;
             }
         }
         return result;
